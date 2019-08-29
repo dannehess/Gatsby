e2ac8d3ad15d522d0b595b77f2c55a5799158a22
Daniel Hessling <daniel.hessling@gmail.com>
2019-08-29 17:46:20 +0200

    Chart

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import styles from "../pages/main-modules.css"
+import Chart from '../components/Chart';
 
 
 export default ({data}) => (
@@ -12,11 +13,13 @@ export default ({data}) => (
         <p>
             My name is Daniel Hessling and I am currently studying Front-End Development at Nackademin in Stockholm, Sweden.
         </p>
-        </section>
-        <section className={styles.section}>
         <p>
 The purpose of this website is to show who I am, projects I have done by myself or being a part of, at Nackademin.
 </p>
+        </section>
+        <section className={styles.section}>
+
+<Chart />
 </section>
 </div>
 </Layout>
